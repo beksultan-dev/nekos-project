5abcb1e037f319b57bace2b3af85df401e4fda57
Beksultan <zxc.beksultan@gmail.com>
2024-01-11 09:41:52 +0600

    test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import { HomePage } from '../pages/home-page';
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../pages/home-page";
 
 function App() {
-    return (
-        <div>
-            <h1>Nav</h1>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </div>
-    );
+	return (
+		<div>
+			<h1>Nav</h1>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
